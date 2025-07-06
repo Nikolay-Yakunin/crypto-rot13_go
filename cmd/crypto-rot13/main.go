fe50b52e4da66c1c37b56441503cb873bfdac780
Nikolay-Yakunin <akuninn52@gmail.com>
2025-07-06 16:27:29 +0700

    add server

---
+++ cmd/crypto-rot13/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	// Тут бы корс добавить
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "crypto-rot13_go is running")
+	})
+
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.Header("Content-Type", "image/x-icon")
+		c.File("../../static/favicon.ico")
+	})
+
+	r.GET("/status", func(c *gin.Context) {
+		var memStats runtime.MemStats
+		runtime.ReadMemStats(&memStats)
+		c.JSON(http.StatusOK, gin.H{
+			"sys time":   memStats.Sys,
+			"alloc":      memStats.Alloc,
+			"total alloc": memStats.TotalAlloc,
+			"num goroutines": runtime.NumGoroutine(),
+			"cpu":        runtime.NumCPU(),
+			"go version": runtime.Version(),
+		})
+	})
+
+	// r.GET("prometheus", func(c *gin.Context) {
+		
+	// }
+
+	return r;
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r := setupRouter()
+	r.Run(":" + port)
+}
