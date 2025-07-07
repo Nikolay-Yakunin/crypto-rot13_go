ad5b40ffd58f5aac7e99c67c7652756e346f29ad
Nikolay-Yakunin <akuninn52@gmail.com>
2025-07-07 09:46:04 +0700

    ref

--- cmd/crypto-rot13/main.go
+++ cmd/crypto-rot13/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"runtime"
 	"strings"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -17,46 +16,21 @@ import (
 	"github.com/Nikolay-Yakunin/crypto-rot13_go/pkg/middleware"
 )
 
-// TODO: Prometheus metrics
-// TODO: Logging
-// TODO: Fix env
-
-func corsInit() cors.Config {
-	config := cors.DefaultConfig()
-
-	config.AllowOrigins = []string{"http://localhost:3000"}
-
-	// Read CORS_ORIGINS from environment variable
-	// and split by comma to allow multiple origins
-	// Example: CORS_ORIGINS=http://example.com,http://another-example.com
-	origins := os.Getenv("CORS_ORIGINS")
-	if origins != "" {
-		for _, origin := range strings.Split(origins, ",") {
-			origin = strings.TrimSpace(origin)
-			if origin != "" {
-				config.AllowOrigins = append(config.AllowOrigins, origin)
-			}
-		}
+func setupMiddleware(r *gin.Engine) {
+	c := middleware.Cors{
+		Origins: strings.Split(os.Getenv("CORS_ORIGINS"), ","),
 	}
+	r.Use(cors.New(c.CorsInit()))
 
-	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
-	config.ExposeHeaders = []string{"Content-Length"}
-	config.AllowCredentials = true
-	config.MaxAge = 12 * time.Hour
+	middleware.PrometheusInit() // Я просто скопировал этот миддлвар, так что он не на типах
+	r.Use(middleware.TrackMetrics())
 
-	return config
 }
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	// Миддлвар для CORS
-	r.Use(cors.New(corsInit()))
-
-	// Миддлвар для метрик Prometheus
-	middleware.PrometheusInit()
-	r.Use(middleware.TrackMetrics())
+	setupMiddleware(r)
 
 	// Эндпоинт для проверки запуска
 	r.GET("/", func(c *gin.Context) {
---
+++ pkg/middleware/cors.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+
+	"time"
+)
+
+type Cors struct {
+	cors.Config
+	Origins []string
+}
+
+func (c Cors)CorsInit() cors.Config {
+	config := cors.DefaultConfig()
+
+	config.AllowOrigins = c.Origins
+
+	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
+
+	return config
+}
